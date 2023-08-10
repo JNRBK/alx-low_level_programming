2679ecf9aa341b087d8c83510b34c37908c5485d
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-10 11:33:51 -0700

    puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,10 @@
-#!/bin/bash
-puts("Programming is like building a multilingual puzzle\n");	
+#include <stdio.h>
+/**
+ * main - this main function
+ * Return: always zero
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
