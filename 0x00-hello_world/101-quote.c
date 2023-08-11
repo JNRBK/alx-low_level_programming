8a1e8babd9bc004c1b8131b2fa792ca5f80111ed
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-10 18:07:34 -0700

    return 1

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main - main function
+ * prints sentence with special character
+ * Return: this will return 1
+ */
+int main(void)
+{
+	printf("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
