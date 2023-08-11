65583b632db16b44981f8a1b90c73e69dcb92296
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-11 07:15:46 -0700

    positive and negative

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - function is main
+ * print if positive or negative or equal
+ * Return: always zero
+ */
+
+int main(void)
+{
+	int n = 0;
+
+	if (n == 0)
+	{
+		printf("0 is zero\n,");
+	}
+	else if (n > 0)
+	{
+		printf("100 is positive\n,");
+	}
+	else if (n < 0)
+	{
+		printf("-100 is negative\n,");
+	}
+	return (0);
+}
