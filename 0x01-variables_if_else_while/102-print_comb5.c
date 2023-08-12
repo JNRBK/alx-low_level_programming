67c6aa825b4d4ae690d3f0a184dfe0ff8498a15c
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-11 20:01:46 -0700

    all possible combinations to 2 digits

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - the main function
+ *
+ * Return: zero always
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = 0; x <= 99; x++)
+	{
+		for (y = x + 1; y <= 99; y++)
+		{
+			putchar((x / 10) + '0');
+			putchar((x % 10) + '0');
+			putchar(' ');
+			putchar((y / 10) + '0');
+			putchar((y % 10) + '0');
+
+			if (x < 98 || y < 99)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
