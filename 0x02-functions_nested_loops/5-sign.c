a52036c883a1837d2148e796fa8b6e414e73c1bb
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-15 19:56:45 -0700

    sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,3 +1,25 @@
-nclude "main.h"
+#include "main.h"
+/**
+ * print_sign - prints a sign
+ * @n: int input
+ * Return: 0 \ 1 \ -1
+ */
+
 int print_sign(int n)
 {
+	if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
+}
