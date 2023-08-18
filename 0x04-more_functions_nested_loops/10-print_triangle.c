54c0b0e95e4bfb12f74678680d56a7b7e512125d
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-18 11:20:46 -0700

    print triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+/**
+ * print_triangle - prints
+ * @size: int
+ * Return: 0
+ */
+
+void print_triangle(int size)
+{
+	int a, b;
+
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (a = 1; a <= size; a++)
+		{
+			for (b = 1; b <= size; b++)
+			{
+				if (b >= size - (a - 1) && b <= size + (a - 1))
+				{
+					_putchar('#');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
