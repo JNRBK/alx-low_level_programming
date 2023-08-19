9f2066400c53532fe33c3477c41eb28b70db85d5
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-18 18:21:50 -0700

    print diagonal

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -9,26 +9,21 @@ void print_diagonal(int n)
 {
 	int a, b;
 
-	if (n <= 0)
+	if (n > 0)
 	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (a = 0; a <= n; a++)
+		for (a = 0; a < n; a++)
 		{
-			for (b = 0; b <= n; b++)
+			for (b = 0; b < a; b++)
 			{
-				if (b == a)
-				{
-					_putchar('\\');
-				}
-				else if (b < a)
-				{
-					_putchar(' ');
-				}
+				_putchar(' ');
 			}
+			_putchar('\\');
 			_putchar('\n');
 		}
 	}
+	else
+	{
+		_putchar('\n');
+	}
 }
+
