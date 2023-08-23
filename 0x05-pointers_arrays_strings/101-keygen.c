e845b74d8038a6dea73856bece94ed2eb72bc862
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-22 18:15:22 -0700

    generate pass

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,16 +1,49 @@
-#include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
-int main(void)
+#include <time.h>
+
+void generatepassword(int n)
 {
+	int i, randomizer = 0;
 	srand(time(NULL));
-	int pass[50];
-	int i;
+	char password[n];
 
-	for (i = 0; i < 50; i++)
+	char numbers[] = "0123456789";
+	char letters[] = "abcdefghijklmnopqrstuvwxyz";
+	char LETTERS[] = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	char symbols[] = "!@#$^&*?";
+
+	randomizer = rand() % 4;
+
+	for (i = 0; i < n; i++)
 	{
-		pass[i] = rand() % 10;
-
+		if (randomizer == 1)
+		{
+			password[i] = numbers[rand() % 10];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 2)
+		{
+			password[i] = letters[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else if (randomizer == 3)
+		{
+			password[i] = LETTERS[rand() % 26];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
+		else
+		{
+			password[i] = symbols[rand() % 8];
+			randomizer = rand() % 4;
+			printf("%c", password[i]);
+		}
 	}
+}
+int main()
+{
 	return (0);
 }
