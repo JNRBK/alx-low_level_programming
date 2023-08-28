f08da54ce231174d805a4f4c72d10b983ed27288
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-29 00:12:55 +0300

    memset

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - function to fill memory with constant byte
+ * @s: char
+ * @b: char
+ * @n: int
+ * Return: s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int r;
+
+	for (r = 0; r < n; r++)
+	{
+		s[r] = b;
+	}
+	return (s);
+}
