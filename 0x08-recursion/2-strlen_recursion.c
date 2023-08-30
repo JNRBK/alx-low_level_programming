3980df6079838b8f440cfab0d17a7c7a95d79a3f
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-08-31 00:56:58 +0300

    strlen recurs

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * _strlen_recursion - get length of string
+ * @s: char
+ * Return: 0
+ */
+
+int _strlen_recursion(char *s)
+{
+	if (*s == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
