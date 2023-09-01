ac250b87fa9fedbb91020832b7e4358ed5697c69
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-09-01 20:09:58 +0300

    my new name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+/**
+ * main - main function
+ * @argc: number of command line arguments
+ * @argv: array of string containing arguments
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc)
+	{
+	printf("%s\n", argv[0]);
+	}
+	return (0);
+}
