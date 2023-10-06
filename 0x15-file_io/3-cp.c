16f1b038f8e9e9245ef57517f51defab496f46f7
ELRAFEEK <rafeekmorgan@yahoo.com>
2023-10-07 01:00:45 +0300

    copy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,37 +1,63 @@
 #include "main.h"
+#define BUFFER_SIZE 1024
 
-void error(int file_to, int file_from)
+/**
+ * _close - close opened files
+ * @fd: file descriptor
+ * @argv: argument victor passed in array
+ * Return: void
+*/
+void _close(ssize_t fd, char *argv)
 {
-    if (file_from == NULL)
-    {
-        dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", file_from)
-        exit(98);
-    }
-    if (file_to == NULL)
-    {
-        dprintf(STDERR_FILENO,"Error: Can't read from file %s\n", file_to);
-        exit(99);
-    }
+	if (close(fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %s\n", argv);
+		exit(100);
+	}
 }
+/**
+ * main - main function
+ * @argc: argument coun
+ * @argv: argument victor
+ * Return: 0
+*/
 int main(int argc, char *argv[])
 {
-    const char *file_from = argv [1];
-    const char *file_to = argv[2];
-    char buffer [1024];
-    int r, b, k;
+	ssize_t r, w, fd1, fd2;
+	char buffer[BUFFER_SIZE];
 
-    if (argc != 3)
-    {
-        dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
-        exit(97);
-
-    }
-
-    a = open(file_from, O_RDONLY);
-
-    b = open(file_to, O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 00664);
-
-
-
-
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	fd1 = open(argv[1], O_RDONLY);
+	if (fd1 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	fd2 = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 00664);
+	if (fd2 == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		exit(99);
+	}
+	while ((r = read(fd1, buffer, BUFFER_SIZE)) > 0)
+	{
+		w = write(fd2, buffer, r);
+		if (w <= 0)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	if (r == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	_close(fd1, argv[1]);
+	_close(fd2, argv[2]);
+	return (0);
 }
