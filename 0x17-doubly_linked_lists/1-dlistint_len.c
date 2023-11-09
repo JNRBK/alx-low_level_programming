b6e0a84301ececb2ac7ea074041f303c29e7eb4b
JNRBK <rafeekmorgan@yahoo.com>
2023-11-09 18:53:32 +0200

    dll-1

---
+++ 0x17-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+/**
+ * dlistint_t - print the elements length
+ * @h: pointer to head
+ * Return: length
+*/
+size_t dlistint_len(const dlistint_t *h)
+{
+	int count = 0;
+
+	while (h != NULL)
+	{
+		h = h->next;
+		count++;
+
+	}
+	return (count);
+}
