2089b73dc5430a80ca46ac1db550001b8449571a
JNRBK <rafeekmorgan@yahoo.com>
2023-11-09 20:25:44 +0200

    dll-4

--- 0x17-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,33 +1,39 @@
 #include "lists.h"
+/**
+ * add_dnodeint_end - add new node at the end of the list
+ * @head: double pointer to the head
+ * @n: const int to data
+ * Return: End node
+*/
 dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-    dlistint_t *End;
-    dlistint_t *pre = *head;
+	dlistint_t *End;
+	dlistint_t *pre = *head;
 
-    End = malloc(sizeof(dlistint_t));
+	End = malloc(sizeof(dlistint_t));
 
-    if (End == NULL)
-    {
-        return (NULL);
-    }
+	if (End == NULL)
+	{
+		return (NULL);
+	}
 
-    End->n = n;
-    End->next = NULL;
-    End->prev = NULL;
+	End->n = n;
+	End->next = NULL;
+	End->prev = NULL;
 
-    if (*head == NULL)
-    {
-        *head = End;
-    }
-    else
-    {
-        while (pre->next != NULL)
-        {
-            pre = pre->next;
-        }
-        pre->next = End;
-        End->prev = pre;
-    }
+	if (*head == NULL)
+	{
+		*head = End;
+	}
+	else
+	{
+		while (pre->next != NULL)
+		{
+			pre = pre->next;
+		}
+		pre->next = End;
+		End->prev = pre;
+	}
 
-    return (End);
+	return (End);
 }
