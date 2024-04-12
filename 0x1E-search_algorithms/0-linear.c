b79bf5e2f06cc847e93f6e24751ad6a9bb74b24d
JNRBK <rafeekmorgan@yahoo.com>
2024-04-12 23:08:51 +0200

    all

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,14 @@
+#include "search_algos.h"
+
+int linear_search(int *array, size_t size, int value)
+{
+    size_t x;
+
+    if (!array || size == 0)
+        return (-1);
+    
+    for (x = 0; x < size; x++)
+    {
+        
+    }
+}
---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,6 @@
+#ifndef ALGOS_H
+#define ALGOS_H
+
+int linear_search(int *array, size_t size, int value);
+
+#endif
