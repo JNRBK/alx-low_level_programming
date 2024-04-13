384c5c64d96cf1d4940ed2f4af7d26c746bbb70f
JNRBK <rafeekmorgan@yahoo.com>
2024-04-13 03:44:17 +0200

    try linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,14 +1,25 @@
 #include "search_algos.h"
-
+/**
+ * linear_search - linear search algorithms
+ * @array: array holding values to search in
+ * @size: size of the array
+ * @value: integer representing the target searched for
+ * Return: value or -1
+*/
 int linear_search(int *array, size_t size, int value)
 {
-    size_t x;
+	size_t x;
 
-    if (!array || size == 0)
-        return (-1);
-    
-    for (x = 0; x < size; x++)
-    {
-        
-    }
+	if (!array || size == 0)
+		return (-1);
+
+	for (x = 0; x < size; x++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", x, array[x]);
+		if (array[x] == value)
+		{
+			return (x);
+		}
+	}
+	return (-1);
 }
--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,6 +1,14 @@
-#ifndef ALGOS_H
-#define ALGOS_H
+#ifndef MAIN_H
+#define MAIN_H
 
+
+/*headers*/
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+
+
+/*prototypes*/
 int linear_search(int *array, size_t size, int value);
 
 #endif
